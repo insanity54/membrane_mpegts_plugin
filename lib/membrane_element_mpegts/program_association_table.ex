c5b94e7ffa6850ff80af27152413a2555513f5f5
Hajto Jakub <hajtosek@gmail.com>
2019-08-23 16:45:13 +0200

    Added program association table parser.

---
+++ lib/membrane_element_mpegts/program_association_table.ex
@@ -0,0 +1,18 @@
+defmodule Membrane.Element.MpegTS.ProgramAssociationTable do
+  @type entry :: %{
+          program_number: 0..65535,
+          program_map_pid: 0..8191
+        }
+
+  @spec parse(binary) :: {:ok, [entry]} | {:error, :malformed_data}
+  def parse(data) when rem(byte_size(data), 4) == 0 do
+    programs =
+      for <<program_number::16, _reserved::3, pid::13 <- data>> do
+        %{program_number: program_number, program_map_pid: pid}
+      end
+
+    {:ok, programs}
+  end
+
+  def parse(_), do: {:error, :malformed_data}
+end
---
+++ test/mpeg_ts/program_association_table_test.exs
@@ -0,0 +1,17 @@
+defmodule Membrane.Element.MpegTS.ProgramAssociationTableTest do
+  use ExUnit.Case
+
+  alias Membrane.Element.MpegTS.Support.Fixtures
+  alias Membrane.Element.MpegTS.ProgramAssociationTable
+
+  describe "Program association table parser" do
+    test "parses valid packet" do
+      assert {:ok, [mapping]} = ProgramAssociationTable.parse(Fixtures.pat())
+      assert mapping == %{program_map_pid: 4096, program_number: 1}
+    end
+
+    test "returns an error when data is not valid" do
+      assert {:error, :malformed_data} = ProgramAssociationTable.parse(<<123, 32, 22, 121, 33>>)
+    end
+  end
+end
