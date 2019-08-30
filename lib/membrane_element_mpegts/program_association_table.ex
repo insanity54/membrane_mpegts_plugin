1157645e9443da046885314c435fcc1f75a22069
Hajto Jakub <hajtosek@gmail.com>
2019-08-30 16:25:13 +0200

    Added PAT parser.

--- lib/membrane_element_mpegts/program_association_table.ex
+++ lib/membrane_element_mpegts/program_association_table.ex
@@ -4,15 +4,18 @@ defmodule Membrane.Element.MpegTS.ProgramAssociationTable do
           program_map_pid: 0..8191
         }
 
-  @spec parse(binary) :: {:ok, [entry]} | {:error, :malformed_data}
+  @spec parse(binary) :: {:ok, map()} | {:error, :malformed_data}
   def parse(data) when rem(byte_size(data), 4) == 0 do
     programs =
       for <<program_number::16, _reserved::3, pid::13 <- data>> do
-        %{program_number: program_number, program_map_pid: pid}
+        {program_number, pid}
       end
+      |> Enum.into(%{})
 
     {:ok, programs}
   end
 
-  def parse(_), do: {:error, :malformed_data}
+  def parse(_) do
+    {:error, :malformed_data}
+  end
 end
