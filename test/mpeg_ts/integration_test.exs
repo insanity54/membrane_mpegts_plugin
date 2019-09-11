af53ae6acacd25df2f125623f5e3be7a9190a2a4
Hajto Jakub <hajtosek@gmail.com>
2019-09-11 19:07:42 +0200

    Added integration test.

---
+++ test/mpeg_ts/integration_test.exs
@@ -0,0 +1,37 @@
+defmodule Membrane.Element.MpegTS.IntegrationTest do
+  use ExUnit.Case
+
+  import Membrane.Testing.Assertions
+
+  alias Membrane.Testing.Pipeline
+
+  @input_path "test/support/all_packets.ts"
+  @reference_audio "test/support/reference_audio.ts"
+  @reference_video "test/support/reference_video.ts"
+  @audio_out "/tmp/audio"
+  @video_out "/tmp/video"
+
+  test "Demuxer splits incoming stream into elementary streams" do
+    options = %Pipeline.Options{
+      module: Membrane.Element.MpegTS.Support.MockPipeline,
+      custom_args: %{
+        inptu_path: @input_path,
+        audio_out: @audio_out,
+        video_out: @video_out
+      }
+    }
+
+    {:ok, pipeline} = Pipeline.start_link(options)
+    Pipeline.play(pipeline)
+    assert_end_of_stream(pipeline, :video_out)
+    assert_end_of_stream(pipeline, :audio_out)
+    assert_files_equal(@reference_audio, @audio_out)
+    assert_files_equal(@reference_video, @video_out)
+  end
+
+  defp assert_files_equal(file_a, file_b) do
+    assert {:ok, a} = File.read(file_a)
+    assert {:ok, b} = File.read(file_b)
+    assert a == b
+  end
+end
---
+++ test/support/mock_pipeline.ex
@@ -0,0 +1,39 @@
+defmodule Membrane.Element.MpegTS.Support.MockPipeline do
+  use Membrane.Pipeline
+  alias Membrane.Pipeline.Spec
+
+  @impl true
+  def handle_init(%{
+        inptu_path: input_path,
+        audio_out: audio_out,
+        video_out: video_out
+      }) do
+    elements = [
+      in: %Membrane.Element.File.Source{location: input_path},
+      demuxer: Membrane.Element.MpegTS.Demuxer,
+      audio_out: %Membrane.Element.File.Sink{location: audio_out},
+      video_out: %Membrane.Element.File.Sink{location: video_out}
+    ]
+
+    links = %{
+      {:in, :output} => {:demuxer, :input},
+      {:demuxer, :output, 1} => {:video_out, :input},
+      {:demuxer, :output, 0} => {:audio_out, :input}
+    }
+
+    spec = %Spec{
+      children: elements,
+      links: links
+    }
+
+    {{:ok, spec}, %{}}
+  end
+
+  def handle_notification({:mpeg_mapping, _maping}, _from, state) do
+    mapping = %{{:output, 1} => 256, {:output, 0} => 257}
+    message = {:config_demuxer, mapping}
+    {{:ok, forward: {:demuxer, message}}, state}
+  end
+
+  def handle_notification(_notification, _from, state), do: {:ok, state}
+end
