e7bbe6f7a5e425db48e77b9b6230c9c6d5aebbe9
Hajto Jakub <hajtosek@gmail.com>
2019-09-11 19:06:18 +0200

    Updated to new core.

--- mix.exs
+++ mix.exs
@@ -52,10 +52,11 @@ defmodule Membrane.Element.MpegTS.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.3.0"},
+      {:membrane_core, github: "membraneframework/membrane-core", override: true},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:membrane_element_file, "~> 0.2.0", only: :test}
     ]
   end
 end
